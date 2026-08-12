a16302e7fe227fd52f8530019d7bcb67b12431f8
Carlos Arcos <carlos.arcos@outlook.com>
2026-08-12 19:31:04 +0200

    Aceptando argumentos de linea de comandos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env; //permite que el programa lea cualquier argumento de línea de comandos.
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+    
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
