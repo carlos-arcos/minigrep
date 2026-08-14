f5b6f161bfdf367fccfba68cef38a788b700f45d
Carlos Arcos <carlos.arcos@outlook.com>
2026-08-14 11:42:29 +0200

    previo a refactorizar con lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
 use std::env; //permite que el programa lea cualquier argumento de línea de comandos.
 use std::fs; //permite que el programa lea archivos del sistema de archivos.
+use std::process; //permite que el programa finalice con un código de salida.
+use std::error::Error; 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_to_string(config.file_path)?;
+
+        println!("With text:\n{contents}");
     
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    Ok(())
+}
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    println!("File contents:\n{contents}");
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
